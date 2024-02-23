28708593e02fbe7f89d1500d932789fc9098c038
Cory Trimm <cory.trimm@gmail.com>
2024-02-23 11:32:12 -0300

    foolish mistake.

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,12 +1,11 @@
 import type { SiteConfig } from "@/types";
-import ogImage from "/og-image.png";
 
 export const siteConfig: SiteConfig = {
   name: "CO2 Web Emissions Tracker",
   description:
     "A simple app built to track carbon emissions over time of popular federal, state, & local government websites.",
   url: "https://ctrimm.github.io/co2-emission-tracker/",
-  ogImage,
+  ogImage: "https://ctrimm.github.io/co2-emission-tracker/og.jpg",
   links: {
     twitter: "https://twitter.com/cdt5058",
     github: "https://github.com/ctrimm/co2-emission-tracker",
