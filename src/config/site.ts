3f12cc3c49f9ab998e7a20e99310802ffb3735be
Cory Trimm <cory.trimm@gmail.com>
2024-02-23 11:26:10 -0300

    Update OG social share image

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,11 +1,12 @@
 import type { SiteConfig } from "@/types";
+import ogImage from "@/images/og.jpg";
 
 export const siteConfig: SiteConfig = {
   name: "CO2 Web Emissions Tracker",
   description:
     "A simple app built to track carbon emissions over time of popular federal, state, & local government websites.",
   url: "https://ctrimm.github.io/co2-emission-tracker/",
-  ogImage: "https://astro-nomy.vercel.app/og.jpg",
+  ogImage,
   links: {
     twitter: "https://twitter.com/cdt5058",
     github: "https://github.com/ctrimm/co2-emission-tracker",
