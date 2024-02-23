dec21660736826b6b097a5ced4c8e2e7b1174c0d
Cory Trimm <cory.trimm@gmail.com>
2024-02-23 01:20:16 -0300

    Clean Up UI - Part One

--- src/config/marketing.ts
+++ src/config/marketing.ts
@@ -2,21 +2,29 @@ import type { MarketingConfig } from "@/types"
 
 export const marketingConfig: MarketingConfig = {
   mainNav: [
+    // {
+    //   title: "Features",
+    //   href: "/#features",
+    // },
+    // {
+    //   title: "Pricing",
+    //   href: "/pricing",
+    // },
+    // {
+    //   title: "Blog",
+    //   href: "/blog",
+    // },
+    // {
+    //   title: "Documentation",
+    //   href: "/docs/getting-started/",
+    // },
     {
-      title: "Features",
-      href: "/#features",
+      title: "About",
+      href: "/about",
     },
     {
-      title: "Pricing",
-      href: "/pricing",
-    },
-    {
-      title: "Blog",
-      href: "/blog",
-    },
-    {
-      title: "Documentation",
-      href: "/docs/getting-started/",
+      title: "FAQ",
+      href: "/faq",
     },
   ],
 }
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,13 +1,13 @@
 import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-  name: "Astronomy",
+  name: "CO2 Web Emissions Tracker",
   description:
-    "An modern example app built using Astro v4 & shadcn/ui. Inspired by shadcn/taxonomy.",
-  url: "https://astro-nomy.vercel.app",
+    "A simple app built to track carbon emissions over time of popular federal, state, & local government websites.",
+  url: "https://ctrimm.github.io/co2-emission-tracker/",
   ogImage: "https://astro-nomy.vercel.app/og.jpg",
   links: {
-    twitter: "https://twitter.com/miickasmt",
-    github: "https://github.com/mickasmt/astro-nomy",
+    twitter: "https://twitter.com/cdt5058",
+    github: "https://github.com/ctrimm/co2-emission-tracker",
   },
 };
