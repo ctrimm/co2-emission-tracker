7d033598971aefcda1d60d0026f6438ec5a54f39
Cory Trimm <cory.trimm@gmail.com>
2024-05-28 14:13:10 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
+// import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 // import vercel from '@astrojs/vercel/static';
